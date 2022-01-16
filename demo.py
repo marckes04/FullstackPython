69c618138c0b1b5fcc84ba82d3ab3c7ddc1586cd
marckes04 <maresp95@gmail.com>
2022-01-15 23:46:10 -0500

    Se añadio Archivo

--- demo.py
+++ demo.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET'])
+@app.route('/', methods = ['GET', 'POST'])
 def home():
-    return render_template('index.html')
-
+    if request.method == 'GET':
+        return render_template('index.html', message = 'Aloha, Aloha')
+    else:
+        username  = request.form['usename']
+        password  = request.form['password']
+        if username == 'Gordon' and password == 'Ramsay':
+            return render_template('football.html',message = 'Login Succesful')
+        else:
+            error_message = 'Hint: He curses a lot'
+            return render_template('index.html', message = error_message)
+        
 @app.route('/football', methods = ['GET'])
 def football():
     return render_template('football.html')
 
 if __name__ == '__main__':
     app.run(port = 7000, debug = True)
+    
