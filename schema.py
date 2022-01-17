5b072f5c349a5a5c6c24af18d355a15863cc084d
marckes04 <maresp95@gmail.com>
2022-01-16 20:41:38 -0500

    Se añadio Archivo

---
+++ schema.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+
+from sqlite3.dbapi2 import Cursor
+connection = sqlite3.connect('flask_tut.db', check_same_thread = False)
+cursor = connection.cursor()
+
+cursor.execute(
+    """CREATE TABLE users(
+        pk INTEGER PRIMARY KEY AUTOINCREMENT,
+        username VARCHAR(16),
+        password VARCHAR(32),
+        favorite_color VARCHAR(32)
+    );"""
+    
+)
+
+connection.commit()
+cursor.close()
+connection.close()
