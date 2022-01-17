5bc57f9799dfd3b5c36c63654c860767491394a4
marckes04 <maresp95@gmail.com>
2022-01-16 21:59:56 -0500

    Se añadio Archivo

--- demo.py
+++ demo.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+import model
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
@@ -9,8 +9,12 @@ def home():
     else:
         username  = request.form['usename']
         password  = request.form['password']
-        if username == 'Gordon' and password == 'Ramsay':
-            return render_template('football.html',message = 'Login Succesful')
+        db_password = model.check_pw(username)
+        
+        
+        if password == db_password:
+            message = model.show_color(username)
+            return render_template('football.html',message = message)
         else:
             error_message = 'Hint: He curses a lot'
             return render_template('index.html', message = error_message)
